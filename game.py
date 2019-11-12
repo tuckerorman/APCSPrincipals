c82cf77998a23cfe7b98b9c42539a9cac8164fd5
tuckerorman <57371200+tuckerorman@users.noreply.github.com>
2019-11-12 11:52:27 -0800

    Update game.py

--- game.py
+++ game.py
@@ -1,26 +1,41 @@
+usersuccess=["-""-""-""-"]
 import random
-rnum=random.randint(1000, 9999)
-print(rnum)
-strnum=str(rnum)
-guesslist= []
-rnumlist= []
+rnum = random.randint (1000, 9999)
+print (rnum)
+#guesses vriable
+guesses = 5
+
+strnum = str (rnum)
+rnumlist = []
+guesslist = []
 for digit in strnum:
     rnumlist.append (int(digit))
+print (rnumlist)
 
-userinput1 = input("input 4 numbers")
-if int(userinput1) == rnum:
-    print("correct")
-print(rnumlist)
+print(usersuccess)
 
-for digit in strnum:
-    rnumlist.append (int(digit))
 
-for i in rnumlist:
-    for j in guesslist:
-     if i == j:
-            print(rnumlist.index(i))
+
+
+#nothing for now
+while guesses != 0:
+    userinput1 = input ("Enter 4 numbers: ")
+
+    for digit in userinput1 :
+        guesslist.append(int(digit))
+    if guesslist == rnumlist:
+        print("Congrats")
+        guesses = 0
+
     else:
-        print ("not correct")
+        for i in rnumlist:
+            for j in guesslist:
+                if i == j :
+                    print (rnumlist.index(i))
+                else:
+                    print ("incorrect")
+    guesslist.clear ()
+
 
 
 
